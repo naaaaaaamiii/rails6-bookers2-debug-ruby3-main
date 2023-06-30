ff12a7b5bc071a5e23336e3a189b410cc038afda
naaaaaaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-06-30 06:58:57 +0000

    コメント終わり

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
-  has_one_attached :profile_image
+ # has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
@@ -9,6 +9,6 @@ class Book < ApplicationRecord
   
    def favorited_by?(user)
     favorites.exists?(user_id: user.id)
-  end
+   end
   
 end
