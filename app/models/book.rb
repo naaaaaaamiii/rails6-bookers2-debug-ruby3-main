d86165767ae353eb88d2af73f522075c691d2042
naaaaaaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-07-02 02:32:25 +0000

    フォロー

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
  # has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationships, dependent: :destroy
   
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,26 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationship, class_name: "relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
-
+  
  # validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :name, length: { in: 2..20 }
   validates :name, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
