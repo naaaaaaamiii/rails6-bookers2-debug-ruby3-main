924689e83c761edbc6ff873d8a28c3bb4d7843a7
naaaaaaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-07-03 01:37:05 +0000

    method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,26 +8,29 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
   has_many :relationship, class_name: "relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
+  has_many :following_users, through: :followers, source: :followed
+  has_many :follower_users, through: :followeds, source: :follower  
   
  # validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :name, length: { in: 2..20 }
   validates :name, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
+
   def follow(user_id)
-    relationships.create(followed_id: user_id)
+    followers.create(followed_id: user_id)
   end
-  
+
   def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+    followers.find_by(followed_id: user_id).destroy
   end
-  
+ 
   def following?(user)
-    followings.include?(user)
-  end
+    following_users.include?(user)
+  end	 
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
