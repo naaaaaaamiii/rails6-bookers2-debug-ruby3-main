7382bba6cc5b6387eed27a9ee8de4966e38aacf7
naaaaaaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-07-04 13:41:29 +0000

    コメント

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
+   # redirect_to request.referer
   end
   
   def destroy
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.destroy
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.post_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.destroy
+   # redirect_to book_path(book)
   end
   
   private
