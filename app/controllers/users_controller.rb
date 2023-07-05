fc385f8987ba5efa4b71c94e18bf5233369abe61
naaaaaaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-07-05 05:59:10 +0000

    投稿数の比較

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books
+    #フォロー
     @following_users = @user.following_users
     @follower_users = @user.follower_users
+    # 投稿数の比較
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
